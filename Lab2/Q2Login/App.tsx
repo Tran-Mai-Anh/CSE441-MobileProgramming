82f6c18e7244e692d33764acdd68e0a06d3c8cb3
Tran-Mai-Anh <155942841+Tran-Mai-Anh@users.noreply.github.com>
2025-05-03 23:14:03 +0700

    Create Q2Login

---
+++ Lab2/Q2Login/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import LoginScreen from './Login/Login';
+
+function App(): React.JSX.Element {
+  return <LoginScreen />;
+}
+
+export default App;
